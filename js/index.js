cea86ee7b4ca224268fb1e6407bdc984bdd7c689
Julesljn <juleslejeunepro@gmail.com>
2024-10-04 11:32:42 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('active');
+            }
+        });
+    });
+
+    const target = document.querySelector('.partnerListC');
+    observer.observe(target);
+});
